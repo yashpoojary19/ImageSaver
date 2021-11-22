c13b008aee1444e6b5c8e5c3e00d802506b5c577
Yash Poojary <yashspoojary245@gmail.com>
2021-11-22 15:02:27 +0530

    Initial Commit

---
+++ ImageSaver/ImageSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSaverApp.swift
+//  ImageSaver
+//
+//  Created by Yash Poojary on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
